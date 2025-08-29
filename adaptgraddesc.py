84e76396bd4a1311372693d0ba4159c4c4de7bd3
Domas Syaifoel <124035858+dmsyaifoel@users.noreply.github.com>
2025-08-29 15:38:20 +0200

    Create adaptgraddesc.py

---
+++ adaptgraddesc.py
@@ -0,0 +1,52 @@
+'''
+Inaccurate but very fast TOPOPT implementation (minimal compliance) using an adaptive gradient descent method
+(it's in fact an adaption of Newton's method, adapted to rootfinding of a function R^n->R, so it doesn't actually converge to a minimum
+Instead it approaches a target compliance (ctarget) and volume fraction (volfrac)
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse import csc_array
+from scipy.sparse.linalg import spsolve
+from scipy.signal import convolve2d
+def topopt(nx, ny, ctarget, volfrac, fixeddofs, forces, maxloops, r=1.5, p=3, E=1, nu=.3, xmin=1e-3):
+  ndof = 2*(nx + 1)*(ny + 1)
+  freedofs = np.setdiff1d(np.arange(ndof), fixeddofs)
+  f = np.zeros(ndof)
+  for force in forces: f[force[0]] = force[1]
+  x = volfrac*np.ones((ny, nx))
+  k = np.array([1/2 - nu/6, 1/8 + nu/8, -1/4 - nu/12, -1/8 + 3*nu/8, -1/4 + nu/12, -1/8 - nu/8, nu/6, 1/8 - 3*nu/8])
+  Ke = np.array([[k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7]], [k[1], k[0], k[7], k[6], k[5], k[4], k[3], k[2]], [k[2], k[7], k[0], k[5], k[6], k[3], k[4], k[1]], [k[3], k[6], k[5], k[0], k[7], k[2], k[1], k[4]], [k[4], k[5], k[6], k[7], k[0], k[1], k[2], k[3]], [k[5], k[4], k[3], k[2], k[1], k[0], k[7], k[6]], [k[6], k[3], k[4], k[1], k[2], k[7], k[0], k[5]], [k[7], k[2], k[1], k[4], k[3], k[6], k[5], k[0]]])*E/(1 - nu**2)
+  _x, _y = np.meshgrid(np.arange(nx), np.arange(ny))
+  _x = _x.ravel()
+  _y = _y.ravel()
+  n1 = (ny + 1)*_x + _y
+  n2 = (ny + 1)*(_x + 1) + _y
+  dofs = np.array([2*n1, 2*n1 + 1, 2*n2, 2*n2 + 1, 2*n2 + 2, 2*n2 + 3, 2*n1 + 2, 2*n1 + 3]).T
+  r_ = int(r) + 1
+  y_, x_ = np.ogrid[-r_:r_+1, -r_:r_+1]
+  kernel = np.maximum(r - np.sqrt(x_**2 + y_**2), 0)
+  d = np.sum(kernel)
+  for i in range(maxloops):
+    K = csc_array(((Ke[None, :, :]*(x.ravel()**p)[:, None, None]).ravel(), (np.repeat(dofs, 8, axis=1).ravel(), np.tile(dofs, (1, 8)).ravel())), shape=(ndof, ndof))
+    u = np.zeros(ndof)
+    u[freedofs] = spsolve(K[freedofs, :][:, freedofs], f[freedofs])
+    uall = u[dofs]
+    call = np.einsum('ni,ij,nj->n', uall, Ke, uall).reshape((ny, nx))
+    c = np.sum(x**p*call) - ctarget
+    if c > 0:
+      dc = p*x**(p - 1)*call
+      dc = convolve2d(x*dc, kernel, mode='same', boundary='symm')/x/d
+      x += dc/np.sum(dc*dc)*c
+    g = np.sum(x) - volfrac*nx*ny
+    if g > 0:
+      dg = np.ones((ny, nx))
+      x -= dg/np.sum(dg*dg)*g
+    x = np.clip(x, xmin, 1)
+    print(f'{i} {c=:3.3} {g=:3.3}')
+  plt.imshow(-x, cmap='gray', interpolation='none')
+  plt.axis('equal')
+  plt.show()
+  return x
+nx, ny = 40, 30
+fixeddofs = np.union1d(np.arange(0, 2*(ny + 1), 2), np.array([2*(nx + 1)*(ny + 1) - 1]))
+x = topopt(nx, ny, 60, .3, fixeddofs, [[1, -1]], 50)
