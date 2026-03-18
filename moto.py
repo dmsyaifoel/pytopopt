08983e3efcb6fcce71036fe87016a16ad5e188be
Domas Syaifoel <d.m.syaifoel@tudelft.nl>
2026-03-18 12:58:11 +0100

    small update

--- moto.py
+++ moto.py
@@ -12,13 +12,16 @@ def cat(a, b, c, d): return jnp.concatenate([a, b, c, d])
 
 timestamp = datetime.now().strftime('%Y%m%d-%H%M')
 
-scale = 5
+scale = 2
 nx = scale*40
 ny = scale*30
 vf = .2
 xmin = 1e-3
 
 domain = pym.VoxelDomain(nx, ny)
+
+domain2 = pym.VoxelDomain(12, 12)
+
 nodes_left = domain.nodes[0, :].ravel()
 fixed_nodes = np.concatenate([nodes_left[:ny//6], nodes_left[5*ny//6:]])
 fixed_dofs = domain.get_dofnumber(fixed_nodes).ravel()
@@ -40,7 +43,7 @@ with pym.Network() as fn:
   vol = pym.EinSum('i->')(x)
   volcon = pym.MathExpression(f'inp0 - {vf*nx*ny}')(vol)
 
-  pym.PlotDomain(domain, saveto = timestamp + '/')(x_filtered)
+  pym.PlotDomain(domain, saveto = timestamp + '/domain')(x_filtered)
 
   x_simp = pym.MathExpression(f'{xmin} + {1 - xmin}*inp0')(x_filtered)
 
@@ -51,6 +54,8 @@ with pym.Network() as fn:
   C = pym.Inverse()(K_condensed)
   C2 = pym.EinSum('ij,ij->ij')(C, C)
 
+  pym.PlotDomain(domain2, saveto = timestamp + '/C2')(C2)
+
   Ctot = pym.EinSum('ij->')(C2)
 
 
@@ -58,46 +63,35 @@ with pym.Network() as fn:
 
   A = np.eye(6)
 
-  diags = []
-  diagsedit = []
-  diagsedittrace = []
-  smallestdiags = []
-  traces = []
-  sums = []
-  offsums = []
-  diagmins = []
-  divs = []
-
-  diags = [pym.EinSum('ij,ij->i')(A, quadrant)) for quadrant in quadrants] # vectors containing the diagonals
-  traces = [pym.EinSum('ij,ij->')(A, quadrant)) for quadrant in quadrants] # traces (scalars, sums of diags)
-  sums = [pym.EinSum('ij->')(A, quadrant)) for quadrant in quadrants]
+  diags = [pym.EinSum('ij,ij->i')(A, quadrant) for quadrant in quadrants] # vectors containing the diagonals
+  smallestdiags = [pym.KSFunction(rho=-1)(diag) for diag in diags]
+  traces = [pym.EinSum('ij,ij->')(A, quadrant) for quadrant in quadrants] # traces (scalars, sums of diags)
+  sums = [pym.EinSum('ij->')(quadrant) for quadrant in quadrants]
   offsums = [pym.MathExpression('inp0 - inp1')(sums[i], traces[i]) for i in range(len(quadrants))]
 
   alldiags = pym.AutoMod(cat)(*diags) # vector of all diagonals
 
-  # smallestalldiags = pym.SoftMinMax(alpha=-10)(alldiags) # smallest value of all diagonals
+  smallestalldiags = pym.KSFunction(rho=-1)(alldiags) # smallest value of all diagonals
 
-  add = ''.join([f'+inp{i}' for i in range(len(quadrants))])
+  add = '0' + ''.join([f'+inp{i}' for i in range(len(quadrants))])
   mul = '1' + ''.join([f'*inp{i}' for i in range(len(quadrants))])
 
   alltraces = pym.MathExpression(add)(*traces)
   alloffsums = pym.MathExpression(add)(*offsums)
-  alldivs = pym.MathExpression(add)(*divs)
-  alldet = pym.MathExpression(add)(*diagsedittrace)
+  # alldivs = pym.MathExpression(add)(*divs)
+  # alldet = pym.MathExpression(add)(*diagsedittrace)
 
-  obj = alldivs
-
-  # obj = pym.MathExpression('inp0/inp1')(alloffsums, alltraces)
+  obj = pym.MathExpression('-inp0')(smallestalldiags)
 
   # obj = pym.MathExpression('inp0/inp1*(inp2+1)')(alloffsums, smallestofalldiags, vol)
   # obj = pym.MathExpression('inp0/inp1')(alloffsums, smallestalldiags)
 
-  # aoscon = pym.MathExpression('inp0 - 1e-3')(alloffsums)
+  aoscon = pym.MathExpression('inp0 - 400')(alloffsums)
   # sd0con = pym.MathExpression('-inp0 + 2000')(smallestdiags[0])
   # sd1con = pym.MathExpression('-inp0 + 5000')(smallestdiags[1])
 
   # obj_con = [vol, sd0con, sd1con]
-  obj_con = [obj]
+  obj_con = [obj, aoscon]
 
   # obj_con = [alloffsums, volcon, pym.MathExpression('-inp0 + 500')(smallestdiags[-1])]
 
@@ -106,4 +100,4 @@ with pym.Network() as fn:
 
 shutil.copy2(__file__, timestamp + '/moto.py')
 
-pym.minimize_mma(x, obj_con, maxit=1000)
+pym.minimize_mma(x, obj_con, maxit=100)
